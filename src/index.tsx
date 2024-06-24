c46d0b2dbcd883fa4f8a6f9e8b978f5955e07e32
Thit Oo <76427440+thitoo-yf@users.noreply.github.com>
2024-06-24 21:06:36 +0630

    feat: add mui theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppTheme from '@/AppTheme'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={AppTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+    
   </React.StrictMode>
 );
 
